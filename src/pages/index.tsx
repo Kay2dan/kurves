3ce225d348110b6bf6ff728a57f14d1b5490e57f
Salman <salman.khan@coutallure.com>
2020-08-04 10:23:52 +0100

    hero section done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Layout } from "../components/index";
+import { Hero, Layout } from "../components/index";
 
 export default function Home() {
   return (
     <Layout>
-      <div>Salam World, this is home!</div>
+      <Hero />
     </Layout>
   );
 }
